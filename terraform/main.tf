b6951b511da00334d418033ffa6784afb5d8d289
al3xdiaz <alexleonel96@hotmail.com>
2022-06-23 13:57:11 -0600

    change: ami id in main.tf terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,8 @@ provider "aws"{
     region = "us-east-1"
 }
 resource "aws_instance" "test" {
-    ami = "ami-0b9c9f62"
+    # ami for Ubuntu 22.04 LTS (HVM), SSD Volume Type
+    ami = "ami-052efd3df9dad4825"
     instance_type = "t2.micro"
     tags = {
         Name = "test-instance"
